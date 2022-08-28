a16093052f45457ed22077305fd0e3a5010cb3fe
seth baldwin <seth.baldwin@teambespin.us>
2022-08-28 07:28:12 -0500

    enabled tracking for 2 different images, does not track simultaniously.

--- AFART/Assets/Scripts/ImageTracking.cs
+++ AFART/Assets/Scripts/ImageTracking.cs
@@ -11,6 +11,7 @@ public class ImageTracking : MonoBehaviour
     [SerializeField]
     private GameObject[] placeablePrefabs;
 
+
     private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
 
     private ARTrackedImageManager trackedImageManager;
@@ -20,10 +21,48 @@ public class ImageTracking : MonoBehaviour
         trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
         foreach(GameObject prefab in placeablePrefabs)
         {
-
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            newPrefab.name = prefab.name;
+            spawnedPrefabs.Add(prefab.name, newPrefab);
         }
     }
 
+    private void OnEnable() {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
 
+    private void OnDisable() {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs) {
+        foreach (ARTrackedImage trackedImage in eventArgs.added) {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated) {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.removed) {
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+            Debug.Log($"{trackedImage.name} isActive set to false");
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage) {
+        string name = trackedImage.referenceImage.name;
+        Vector3 position = trackedImage.transform.position;
+
+        GameObject prefab = spawnedPrefabs[name];
+        prefab.transform.position = position;
+        prefab.SetActive(true);
+         
+        foreach(GameObject gmObj in spawnedPrefabs.Values) {
+            if (gmObj.name != name) { 
+                gmObj.SetActive(false);
+            }
+            Debug.Log($"{gmObj.name} set to inactive");
+        }
+
+    }
 
 }
